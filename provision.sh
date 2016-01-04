6996d36d0c538a9615b63f14e273c3d5ee926b1b
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2016-01-04 15:47:30 +0900

    update script for ruby-2.0.0

--- provision.sh
+++ provision.sh
@@ -12,7 +12,7 @@ tar -xzf ruby-install-0.5.0.tar.gz
 pushd ruby-install-0.5.0/ 
 	make install
 popd
-/usr/local/bin/ruby-install --system ruby 1.9.3 -- --disable-install-rdoc
+/usr/local/bin/ruby-install --system ruby 2.0.0 -- --disable-install-rdoc
 export PATH=$PATH:/usr/local/bin
 which ruby
 
